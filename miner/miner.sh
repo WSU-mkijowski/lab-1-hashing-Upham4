c94c5e22838e5a5b4d35942b2c22b73206a1135c
Upham4 <Kazuyainsmash3@outlook.com>
2025-09-04 00:45:48 +0000

    added miner code

--- miner/miner.sh
+++ miner/miner.sh
@@ -3,20 +3,10 @@
 
 echo "Starting this script (hopefully it is executable with chmod a+x ./miner.sh)"
 
-VARIABLE=some_string
-
-echo $VARIABLE
-
-## Prints all words in provided dictionary
-## (you might want to find a bigger dictionary)
-for i in $(cat ../data/dictionary); do
-  printf $i
+for nonce in $(seq 0 999); do
+    for word in $(cat ../data/dictionary); do
+        nonceword="${nonce}${word}"
+        echo -n "$nonceword "
+        echo -n "$nonceword" | sha256sum
+    done
 done
-
-
-## prints all numbers between 100 and 105
-for i in $(seq 100 105); do
-  printf $i
-done
-
-
